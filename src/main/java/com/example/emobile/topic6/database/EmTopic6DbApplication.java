d781a7f9a4da75c79af315b25199e3c59b95a72d
VladislavKolos <vladislav.kolos.y@gmail.com>
2025-05-07 17:37:46 +0300

    Initial commit

---
+++ src/main/java/com/example/emobile/topic6/database/EmTopic6DbApplication.java
@@ -0,0 +1,11 @@
+package com.example.emobile.topic6.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmTopic6DbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmTopic6DbApplication.class, args);
+    }
+}
